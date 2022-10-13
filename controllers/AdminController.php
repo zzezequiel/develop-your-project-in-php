fdccefa9b0fa8070c357d86e4c4c16eb6d58859c
Marta Punset <martapunset10@gmail.com>
2022-10-13 15:44:45 +0200

    correct version

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -5,28 +5,48 @@
     class AdminController{
         use Controller;
 
-    function getAllProducts(){
+        function getAllProducts(){
             $products = $this -> model ->get(); 
 
 
-        if(isset($products)){
-            $this->view->data = $products;
-            $this->view->render("adminView/adminDashboard");      
+            if(isset($products)){
+                $this->view->data = $products;
+                $this->view->render("adminView/adminDashboard");      
+            }
+        
+        
         }
-        
-        
-    }
 
-    function getProduct($request){
+
+
+        function getProduct($request){
         $products = null;
 
-    if (isset($request["id-product"])) {
-        $products = $this->model->getById($request["id-product"]);
-    }
+            if (isset($request["id"])) {
+            $products = $this->model->getById($request["id"]);
+        }
 
-    $this->view->action = $request["action"];
-    $this->view->data = $products;
-    $this->view->render("products/products");
+        $this->view->action = $request["action"];
+        $this->view->data = $products;
+        $this->view->render("adminView/updateProduct");
+        
+        }
 
+
+        function update($request)
+    {
+        if (sizeof($_POST) > 0) {
+            $employee = $this->model->update($_POST);
+
+            if ($employee[0]) {
+                header("Location: index.php?controller=Admin&action=getAllProducts");
+            } else {
+                $this->action = $request["action"];
+                $this->error = "The data entered is incorrect, check that there is no other employee with that email.";
+                $this->view->render("adminView/updateProduct");
+            }
+        } else {
+            $this->view->render("adminView/adminDashboard");
+        }
     }
     }
