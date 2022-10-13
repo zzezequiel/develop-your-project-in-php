a3a3a51124eed792b1352e5b388d43e94ac957fe
Marta Punset <martapunset10@gmail.com>
2022-10-13 13:12:07 +0200

    ClientMOdel repaired

--- models/ClientModel.php
+++ models/ClientModel.php
@@ -1,34 +1,33 @@
 <?php
 
-    class ClientModel extends Model{
+    
+class ClientModel extends Model{
 
-        function get(){
-            $query = $this->db->connect()->prepare("SELECT p.title, ud.`first-name` as 'name', ud.`last-name` as 'lastName', p.location, p.description, p.`pre-build`, p.size, p.price, p.img
-            FROM products p
-            LEFT JOIN userdata ud ON p.`id-user` = ud.id
-            ORDER BY p.id;");
+    function get(){
+        $query = $this->db->connect()->prepare("SELECT p.title, ud.first_name as 'name', ud.last_name as 'lastName', p.location, p.description, p.pre_build, p.size, p.price, p.img
+        FROM products p
+        LEFT JOIN userdata ud ON p.id_user = ud.id
+        ORDER BY p.id;");
 
-            try {
-                $query->execute();
-                $products = $query->fetchAll();
-                return $products;
-            } catch (PDOException $e) {
-                return [];
-            }
+        try {
+            $query->execute();
+            $products = $query->fetchAll();
+            return $products;
+        } catch (PDOException $e) {
+            return [];
         }
-    
-      /*   function getByIdProduct($idProduct){
-            $query = $this->db->connect()->prepare("SELECT p.`id-product`, p.title, p.location, p.description, p.`pre-build`, p.size, p.price 
-            FROM products p
-            WHERE id-products = $idProduct;");
-        
-            try {
-                $query->execute();
-                $idProduct = $query->fetch();
-                return $idProduct;
-            } catch (PDOException $e) {
-                return [];
-            }
-        } */
     }
+
+     function getByIdProduct($id){
+        $query = $this->db->connect()->prepare("SELECT p.title, ud.first_name as 'name', ud.last_name as 'lastName', p.location, p.description, p.pre_build, p.size, p.price, p.img
+        FROM products p
+        WHERE id = $id;");
     
+        try {
+            $query->execute();
+            $id = $query->fetch();
+            return $id;
+        } catch (PDOException $e) {
+            return [];
+        }
+    }
