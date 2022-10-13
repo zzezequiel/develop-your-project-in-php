b620f76f8bd935e4d0e7032436ebf799269971d2
zzezequiel <ezequielzvirgzdins@gmail.com>
2022-10-13 12:52:48 +0200

    correct version

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -28,7 +28,25 @@
 
         $this->view->action = $request["action"];
         $this->view->data = $products;
-        $this->view->render("products/products");
-
+        $this->view->render("adminView/updateProduct");
+        
         }
+
+
+        function update($request)
+    {
+        if (sizeof($_POST) > 0) {
+            $employee = $this->model->update($_POST);
+
+            if ($employee[0]) {
+                header("Location: index.php?controller=Admin&action=getAllProducts");
+            } else {
+                $this->action = $request["action"];
+                $this->error = "The data entered is incorrect, check that there is no other employee with that email.";
+                $this->view->render("adminView/updateProduct");
+            }
+        } else {
+            $this->view->render("adminView/adminDashboard");
+        }
+    }
     }
