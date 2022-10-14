e90bb923caaa9773288ec5b07ec4b13ca170023b
Alex Balaguer <balaguer.dev@gmail.com>
2022-10-14 12:58:57 +0200

    stetic change admin and client

--- models/AdminModel.php
+++ models/AdminModel.php
@@ -3,11 +3,10 @@
     class AdminModel extends Model{
 
         function get(){
-            $query = $this->db->connect()->prepare("SELECT * FROM products
-            /* p.`id-product`, p.title, ui.`first-name`, p.location, p.description, p.`pre-builtd`, p.size, p.price 
-            FROM products p  */
-           /*  INNER JOIN `users-info` ui ON p.`id-product` = ui.`id-product`
-            ORDER BY p.`id-product` ASC */;");
+            $query = $this->db->connect()->prepare("SELECT p.id, p.title, ud.first_name as 'name', ud.last_name as 'lastName', p.location, p.description, p.pre_build, p.size, p.price, p.img
+    FROM products p
+    LEFT JOIN userdata ud ON p.id_user = ud.idd
+    ORDER BY p.id;");
 
             try {
                 $query->execute();
