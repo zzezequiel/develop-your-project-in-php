83a07ac3b0d6550737f642bc83366ea637f4e511
Alvaro Alonso <airuritac@gmail.com>
2023-03-25 14:56:12 +0100

    last

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -64,22 +64,3 @@ class LoginController
 
        }
     }
-
-
-
-
-
-
-
-
-
-
-    // redirect users to a controller
-    
-    // iniciar la sesion y la conexion a bd
-    // Recoger datos del formulario -- meterle un trim($_POST['email']) para que no guarde espacios
-    // consulta para comprobar credenciales del usuario
-    // Comprobar la contraseña / cfrar
-    // Urilizar una sesion para guardar los datos del usuario logueado
-    // Si algo falla enviar una sesion con el fallo
-    // Rediriguir a x.php
